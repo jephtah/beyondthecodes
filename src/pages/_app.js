a1b4ca1edca1fb7354282fd0deb4e9cd46d82866
Abimbola Oladele <bimbooladele@outlook.com>
2023-02-04 20:44:24 +0100

    inialize project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { ThemeProvider } from "styled-components";
+import { theme } from "@/utils";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
---
+++ src/utils/theme.js
@@ -0,0 +1,10 @@
+const colors = {
+  primary: "#F15A29",
+  black: "#2E2E2E",
+  text: "#878787",
+  white: "#FFFFFF",
+};
+
+export const theme = {
+  colors,
+};
